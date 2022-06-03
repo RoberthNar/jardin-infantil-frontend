ff80ba0ab97d9eeab2a99f5221b810899dee2412
Roberth Narvaez <roberth.narvaez@ceiba.com.co>
2022-06-03 08:13:43 -0500

    adicionando pruebas e2e

--- src/app/core/guard/security.guard.ts
+++ src/app/core/guard/security.guard.ts
@@ -2,9 +2,7 @@ import { Injectable } from '@angular/core';
 import { CanActivate, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class SecurityGuard implements CanActivate {
   canActivate(): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     return true;
